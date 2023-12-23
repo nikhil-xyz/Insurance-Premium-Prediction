c52ab826360c41dd6c756d2ca7f3bb223e280384
nikhil-xyz <sonkusarenikhil@sggs.ac.in>
2023-12-23 16:04:37 +0530

    data ingestion and transformation

---
+++ src/components/data_transformation.py
@@ -0,0 +1,104 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object
+from dataclasses import dataclass
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path : str = os.path.join('artifacts', 'preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        try:
+            logging.info('Entered inside the data transformation component')
+
+            numerical_columns = ['age', 'bmi']
+            categorical_columns = ['sex', 'children', 'smoker', 'region']
+
+            logging.info('creating numerical pipeline')
+            numerical_pipeline = Pipeline(
+                steps = [
+                    ('imputer', SimpleImputer(strategy='median')),
+                    ('scaler', StandardScaler())
+                ]
+            )
+
+            logging.info('creating categorical pipeline')
+            categorical_pipeline = Pipeline(
+                steps = [
+                    ('imputer', SimpleImputer(strategy='most_frequent')),
+                    ('encoder', OneHotEncoder()),
+                    ('scaler', StandardScaler(with_mean=False))
+                ]
+            )
+
+            logging.info('Creating preprocessor object (Column Transformer)')
+            preprocessor = ColumnTransformer(
+                [
+                    ('numerical pipeline', numerical_pipeline, numerical_columns),
+                    ('categorical pipeline', categorical_pipeline, categorical_columns)
+                ]
+            )
+
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info('Read train and test data')
+
+            logging.info('obtaining preprocessing object')
+            preprocessor_obj = self.get_data_transformer_object()
+
+
+            target_column_name = 'expenses'
+            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]
+
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
+
+            logging.info('splitting of training and testing data into dependant and independant features is completed.')
+            logging.info('Applying preprocessing object on training and testing dataframe')
+
+            input_feature_train_arr=preprocessor_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessor_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info('Saving the preprocessing object')
+
+            save_object(
+                file_path = self.data_transformation_config.preprocessor_obj_file_path,
+                obj = preprocessor_obj
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+            )
+
+
+        except Exception as e:
+            raise CustomException(e, sys)
